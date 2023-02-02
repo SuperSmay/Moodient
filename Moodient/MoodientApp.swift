02be74602d45faf8e6581cbf76c9e4d218cb9316
Sam Askling <samaskling@gmail.com>
2023-02-02 15:42:08 -0600

    Initial Commit

---
+++ Moodient/MoodientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodientApp.swift
+//  Moodient
+//
+//  Created by Smay on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
