11110b95a8e744bd11644467606153ec2f8d30fb
Sam Askling <samaskling@gmail.com>
2023-02-14 23:19:29 -0600

    Mm much better

--- FullEventView.swift
+++ FullEventView.swift
@@ -17,21 +17,27 @@ struct FullEventView: View {
     
     @State private var month = Date.now
     
+    let springTransition = Animation.spring(dampingFraction: 0.7, blendDuration: 0.5)
     
     /// Drag stuff
     @State private var dragOffset = CGSize.zero
     @State private var changeRatio = 0.0
-    @State private var changeWidth = 0.0
     
     /// Transitions
-    @State private var isTransitioningUp = false
-    @State private var monthID = UUID() /// This is to store the current ID of the month being shown, so that we can change it later and use transitions
+    @State private var isTransitioningUp = false  /// Keep track of which way the transition will happen
+    @State private var monthID = UUID()  /// This is to store the current ID of the month being shown, so that we can change it later and use transitions
     
     /// Button hold stuff
-    @State var timeRemaining = 0.5
-    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    /// https://stackoverflow.com/questions/62239854/how-to-make-a-longpressgesture-that-runs-repeatedly-while-the-button-is-still-be
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    @State var timeRemaining = 0.0
+    @State private var buttonRepeatTime = 0.5
+    @State private var buttonRepeatMonthCount = 1
+    
     @State var userIsPressing = false //detecting whether user is long pressing the screen
     @State private var buttonWasHeld = false
+    @State private var buttonDirectionHeld = MonthDirection.backward
 
     @Environment(\.colorScheme) var colorScheme
     /// The size of the window this view is being displayed in
@@ -40,174 +46,179 @@ struct FullEventView: View {
     var body: some View {
         
         NavigationView {
-            VStack()
-            {
+            
+            VStack {
+                
                 let monthIndex = Calendar.current.dateComponents([.month], from: month).month
                 
-                Button() {
+                Button() { } label: {
                     
-                } label: {
-                    let newDate = Calendar.current.date(byAdding: .month, value: -1, to: month) ?? Date.now
+                    MonthChangeButton(month: month, direction: .backward, fillPercent: changeRatio)
                     
-                    let newIndex = Calendar.current.dateComponents([.month], from: newDate).month
-                    
-                    
-                    Label(Calendar.current.monthSymbols[(newIndex ?? 1) - 1] + " " + String(Calendar.current.component(.year, from: month)), systemImage: "chevron.down")
-                        .padding()
-                        .background(Color.secondary.opacity(0.5))
-                    //.foregroundColor(.primary)
-                        .background {
-                            GeometryReader { geo in
-                                let frameWidth = changeRatio > 0 ? geo.frame(in: .global).width * changeRatio : 0
-                                Color.primary
-                                    .frame(width: frameWidth, alignment: .leading)
-                            }
-                        }
-                        .animation(.easeInOut, value: month)
-                        .animation(.easeInOut, value: changeRatio)
-                        
-                                            
-                
-                        
                 }
-                .clipShape(RoundedRectangle(cornerRadius: 100, style: .continuous))
-                .onReceive(self.timer) { _ in
-                    if self.userIsPressing {
-                        if self.timeRemaining > 0 {
-                            self.timeRemaining -= 0.5
-                        }
-                        //resetting the timer every 0.5 secdonds and executing code whenever //timer reaches 0
-                        
-                        if self.timeRemaining == 0 {
-                            withAnimation(.spring(dampingFraction: 0.7, blendDuration: 0.5)) {
-                                if buttonWasHeld {
-                                    backOneMonth()
-                                } else {
-                                    buttonWasHeld = true
-                                }
-                                
-                            }
-                            self.timeRemaining = 0.5
-                        }
-                    }
-                }
+                .padding(.vertical)
                 .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { value in
                     
+                    /// Reset the remaining until trigger when the drag starts/ends
+                    self.timeRemaining = buttonRepeatTime
                     
+                    buttonDirectionHeld = .backward
+                    
+                    /// Only trigger on release if the button was not held down
                     if !buttonWasHeld && !value {
-
-                        withAnimation(.spring(dampingFraction: 0.7, blendDuration: 0.5)) {
-                            backOneMonth()
-                        }
                         
+                        withAnimation(springTransition) {
+                            changeMonth(by: -1)
+                        }
+   
                     }
                     
                     self.userIsPressing = value
                     
+                    /// If the gesture was released, then the button is no longer being held
                     if !userIsPressing {
+                        buttonRepeatTime = 0.5
+                        buttonRepeatMonthCount = 1
                         buttonWasHeld = false
                     }
-                   
-                        
                     
-                }, perform: {
-                    
-                } )
+                }, perform: { })
                 
                 Spacer()
-                
-                
+                                
                 MonthView(dayInMonth: month)
-                        
-                        
-                        .padding()
-                        .navigationTitle("\(Calendar.current.monthSymbols[(monthIndex ?? 1) - 1]) \(String(Calendar.current.component(.year, from: month)))")
-                            
-                        
-                        .offset(y: dragOffset.height)
-                        
-                        
-                        .gesture(
-                            DragGesture()
-                                .onChanged({ gesture in
-                                    dragOffset = gesture.translation
-                                    changeRatio = gesture.translation.height/mainWindowSize.height * 2 * 2
-                                })
-                                .onEnded({ gesture in
-                                    withAnimation(.spring(dampingFraction: 0.7, blendDuration: 0.5)) {
-                                        if changeRatio >= 1 {
-                                                backOneMonth()
-                                            }
-                                        
-                                        if changeRatio <= -1 {
-                                                forwardOneMonth()
-                                            }
-                                            
-                                        dragOffset = CGSize.zero
-                                        changeRatio = 0
+                    .id(monthID) /// https://sakunlabs.com/blog/swiftui-identity-transitions/ THATS IT
+                    .padding()
+                    .navigationTitle("\(Calendar.current.monthSymbols[(monthIndex ?? 1) - 1]) \(String(Calendar.current.component(.year, from: month)))")
+                    .offset(y: dragOffset.height)
+                    .zIndex(-1)
+                    /// These two make the whole area around the month swipeable
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .contentShape(Rectangle())
+                    /// The drag gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                dragOffset = gesture.translation
+                                changeRatio = gesture.translation.height/mainWindowSize.height * 2 * 2
+                            })
+                            .onEnded({ gesture in
+                                withAnimation(springTransition) {
+                                    if changeRatio >= 1 {
+                                        changeMonth(by: -1)
                                     }
                                     
+                                    if changeRatio <= -1 {
+                                        changeMonth(by: 1)
+                                    }
                                     
-                                        
-                                    
-                                }))
-                        
-                    
-                
-                .id(monthID) /// https://sakunlabs.com/blog/swiftui-identity-transitions/ THATS IT
-                .transition(.asymmetric(insertion: .offset(y: isTransitioningUp ? mainWindowSize.height : mainWindowSize.height * -1), removal: .opacity.combined(with: .scale)))
-                
-                //Text("\(changeRatio)")
+                                    dragOffset = CGSize.zero
+                                    changeRatio = 0
+                                }
+      
+                            }))
+                    .transition(.asymmetric(insertion: .offset(y: isTransitioningUp ? mainWindowSize.height : mainWindowSize.height * -1), removal: .opacity.combined(with: .scale)))
+
                 Spacer()
                 
-                Button() {
-                    withAnimation(.spring(dampingFraction: 0.7, blendDuration: 0.5)) {
-                        forwardOneMonth()
+                let currentMonthAndYear = Calendar.current.dateComponents([.year, .month], from: Date.now)
+                let displayMonthAndYear = Calendar.current.dateComponents([.year, .month], from: month)
+                if (currentMonthAndYear != displayMonthAndYear) {
+                    Button() {
+                        withAnimation(springTransition) {
+                            month = .now
+                            UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+                            /// UUID is not changed, so that the animation is different to indicate that something different changed
+                        }
+                    } label: {
+                        Text("Jump to Today")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 100, style: .continuous))
                     }
-                } label: {
-                    let newDate = Calendar.current.date(byAdding: .month, value: 1, to: month) ?? Date.now
                     
-                    let newIndex = Calendar.current.dateComponents([.month], from: newDate).month
-                    
-                    
-                    Label(Calendar.current.monthSymbols[(newIndex ?? 1) - 1] + " " + String(Calendar.current.component(.year, from: month)), systemImage: "chevron.up")
-                                .padding()
-                                .background(Color.secondary.opacity(0.5))
-                                //.foregroundColor(.primary)
-                                .background {
-                                    GeometryReader { geo in
-                                        let frameWidth = changeRatio < 0 ? geo.frame(in: .global).width * changeRatio * -1: 0
-                                        Color.primary
-                                            .frame(width: frameWidth, alignment: .leading)
-                                    }
-                                }
-                                .animation(.easeInOut, value: month)
-                                .animation(.easeInOut, value: changeRatio)
-                    
-                        
-                }
-                .clipShape(RoundedRectangle(cornerRadius: 100, style: .continuous))
-                
-                
-        }
-        }
-    }
 
+                }
+                
+                Button() { } label: {
+                    
+                    MonthChangeButton(month: month, direction: .forward, fillPercent: changeRatio * -1)
     
-    func backOneMonth() {
-        isTransitioningUp = false
-        monthID = UUID()
-        let newDate = Calendar.current.date(byAdding: .month, value: -1, to: month) ?? Date.now
-        month = newDate
+                }
+                .padding(.vertical)
+                .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { value in
+                    
+                    buttonDirectionHeld = .forward
+                    
+                    /// Reset the remaining until trigger when the drag starts/ends
+                    self.timeRemaining = buttonRepeatTime
+                    
+                    /// Only trigger on release if the button was not held down
+                    if !buttonWasHeld && !value {
+                        
+                        withAnimation(springTransition) {
+                            changeMonth(by: 1)
+                        }
+   
+                    }
+                    
+                    self.userIsPressing = value
+                    
+                    
+                    /// If the gesture was released, then the button is no longer being held
+                    if !userIsPressing {
+                        buttonRepeatTime = 0.5
+                        buttonRepeatMonthCount = 1
+                        buttonWasHeld = false
+                    }
+                    
+                }, perform: { })
+     
+            }
+            /// Make holding button work
+            .onReceive(self.timer) { _ in
+                /// If the user is pressing the button when the timer event is received
+                if self.userIsPressing {
+                    /// If there is time left on the timer, then remove 0.1 seconds
+                    if round(self.timeRemaining * 10)/10.0 > 0 {
+                        self.timeRemaining -= 0.1
+                    }
+                    
+                    /// If the timer reached 0, do a thing
+                    if round(self.timeRemaining * 10)/10.0 <= 0 {
+                        
+                        if round(buttonRepeatTime * 10)/10 > 0.1 {
+                            buttonRepeatTime -= 0.1
+                        } else {
+                            buttonRepeatMonthCount += 1
+                        }
+                        
+                        var monthChange = 1
+                        monthChange += buttonRepeatMonthCount/50
+                        monthChange *= buttonDirectionHeld == .forward ? 1 : -1
+                        
+                        withAnimation(springTransition) {
+                            changeMonth(by: monthChange)
+                        }
+                        
+                        buttonWasHeld = true
+                        
+                        self.timeRemaining = buttonRepeatTime
+                        
+                    }
+                }
+            }
+        }
     }
     
-    func forwardOneMonth() {
-        isTransitioningUp = true
+    func changeMonth(by change: Int) {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        isTransitioningUp = change > 0
         monthID = UUID()
-        let newDate = Calendar.current.date(byAdding: .month, value: 1, to: month) ?? Date.now
+        let newDate = Calendar.current.date(byAdding: .month, value: change, to: month) ?? Date.now
         month = newDate
     }
-    
     
     /// Callback to delete rows from the database when rows are deleted from the UI
     func removeRows(at offsets: IndexSet) {
@@ -225,6 +236,61 @@ struct FullEventView: View {
         moodDays = MoodEventStorage.moodEventStore.getAllMoodDays()
     }
     
+}
+
+struct MonthChangeButton: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    let month: Date
+    let direction: MonthDirection
+    let fillPercent: Double
+    
+    var body: some View {
+        
+        let newDate = Calendar.current.date(byAdding: .month, value: direction == .forward ? 1 : -1, to: month) ?? Date.now
+        let newIndex = Calendar.current.dateComponents([.month], from: newDate).month
+        let monthName = Calendar.current.monthSymbols[(newIndex ?? 1) - 1]
+        let year = Calendar.current.component(.year, from: newDate)
+        
+        Label("\(monthName) \(year.formatted(.number.grouping(.never)))", systemImage: "chevron.\(direction == .forward ? "up" : "down")")
+            .padding()
+            .foregroundColor(.primary)
+            
+            /// Progress bar things
+            .background {
+                /// In .background to avoid resizing view
+                GeometryReader { geo in
+                    /// Calculate the width of the "progress bar"
+                    
+                    
+                    let frameWidth = fillPercent > 0 ? geo.frame(in: .global).width * fillPercent : 0
+                    /// The "progress bar" itself
+                    Color.white.opacity(0.75)
+                        .frame(width: frameWidth, alignment: .leading)
+                    
+                    
+                }
+            }
+            .background(Color.secondary.opacity(0.5))
+            /// Don't apply an animation to the progress thing
+            .animation(nil, value: fillPercent)
+
+            .clipShape(RoundedRectangle(cornerRadius: 100, style: .continuous))
+            /// Fancy stretch
+            
+            .scaleEffect(x: fillPercent > 0 ? 1.0 + fillPercent/30 : 1, anchor: .leading)
+            .shadow(color: colorScheme == .dark ? .white.opacity(0.5) : .black.opacity(0.1), radius: fillPercent > 0 ? 10 : 0)
+            /// Animations
+            .animation(.easeInOut, value: month)
+            .animation(.easeInOut, value: fillPercent)
+    }
+    
+}
+
+enum MonthDirection {
+    case forward
+    case backward
 }
 
 struct FullEventView_Previews: PreviewProvider {
